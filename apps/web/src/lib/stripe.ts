4b4ea614bf0fc4642b47bfd7972ddc22efd3674b
DanSiro <sirodaniel48@gmail.com>
2026-06-03 08:56:18 +0300

    Bug fixes

--- apps/web/src/lib/stripe.ts
+++ apps/web/src/lib/stripe.ts
@@ -1,5 +1,5 @@
 import Stripe from 'stripe';
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '', {
-  apiVersion: '2023-10-16', // or latest
+  apiVersion: '2026-05-27.dahlia', // or latest
 });
